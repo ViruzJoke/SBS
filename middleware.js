8f8b6ac0d13035885112ea53162e01e3522db093
ViruzJoke <h.joke9@gmail.com>
2025-09-05 15:29:53 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,20 +1,23 @@
+import { NextResponse } from 'next/server';
+
 export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
+     * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * - maintenance.html (the maintenance page itself)
+     * - any assets in the /public folder
      */
-    '/((?!_next/static|_next/image|favicon.ico|maintenance.html).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|data|Icon|DBS_Background.jpg|DBS_Icon.png|DBS_Icon2.jpg|DBS_Icon3.jpg|DHL_logo_rgb_black.svg|Server.jpg).*)',
   ],
 };
 
 export function middleware(request) {
   if (process.env.MAINTENANCE_MODE === 'on') {
-    const url = request.nextUrl.clone();
-    url.pathname = '/maintenance.html';
-    return Response.redirect(url);
+    return NextResponse.rewrite(new URL('/maintenance.html', request.url));
   }
+
+  return NextResponse.next();
 }
